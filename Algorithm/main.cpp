bf994928bba25f83bc5bbb79f264c311147621ee
wi-seong-cheol <tjd9248@naver.com>
2021-12-21 18:16:53 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by wi_seong on 2021/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
