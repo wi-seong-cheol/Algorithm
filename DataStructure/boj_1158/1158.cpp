d8878f2c6cff04c188195a8816f61c4d00bdc0ae
wi-seong-cheol <tjd9248@naver.com>
2021-12-21 18:44:29 +0900

    multiple main

--- DataStructure/boj_1158/1158.cpp
+++ DataStructure/boj_1158/1158.cpp
@@ -6,10 +6,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return 0;
 }
