518af30b82b197975ff6bc73830992b8f21197d4
wi-seong-cheol <tjd9248@naver.com>
2021-12-21 19:39:40 +0900

    1588 solve

---
+++ Backjoon/DataStructure/1158.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by wi_seong on 2021/12/21.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N, K; cin >> N >> K;
+    queue<int> Q;
+    
+    for(int i = 1; i <= N; i++) Q.push(i);
+    
+    cout << "<";
+    while(true) {
+        for(int i = 1; i < K; i++) {
+            Q.push(Q.front());
+            Q.pop();
+        }
+        cout << Q.front(); Q.pop();
+        if (!Q.empty()) cout << ", ";
+        else {
+            cout << ">";
+            break;
+        }
+    }
+    
+    return 0;
+}
