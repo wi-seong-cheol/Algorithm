69e2c12fc06bbd18bc0c666919c7e2e558f45288
wi-seong-cheol <tjd9248@naver.com>
2022-01-01 12:43:35 +0900

    14676 solve

--- Backjoon/SJAlgorithm/14676.cpp
+++ Backjoon/SJAlgorithm/14676.cpp
@@ -6,6 +6,8 @@
 //
 
 #include <iostream>
+#include <vector>
+#define MAX 100001
 
 using namespace std;
 
@@ -14,12 +16,50 @@ int main(int argc, const char * argv[]) {
     cin.tie(NULL); cout.tie(NULL);
     
     int N, M, K; cin >> N >> M >> K;
+    int inDegree[MAX], construct[MAX];
+    vector<int> adj[MAX];
     
-    while(K--) {
-        
+    for(int i = 0; i < M; i++) {
+        int U, V; cin >> U >> V;
+        adj[U].push_back(V);
+        inDegree[V]++;
     }
-    cout << "King-God-Emperor" << '\n';
-    cout << "Lier!" << '\n';
+    
+    int op,a;
+    bool pass = true;
+    
+    for(int i = 0; i < K; i++) {
+        cin >> op >> a;
+        if(op == 1) {
+            if(inDegree[a] == 0) {
+                construct[a]++;
+                if(construct[a] == 1) {
+                    for(int j = 0; j < adj[a].size(); j++) {
+                        inDegree[adj[a][j]]--;
+                    }
+                }
+            } else {
+                pass = false;
+            }
+        } else {
+            if(construct[a] > 0) {
+                construct[a]--;
+                if(construct[a] == 0) {
+                    for(int j = 0; j < adj[a].size(); j++) {
+                        inDegree[adj[a][j]]++;
+                    }
+                }
+            } else {
+                pass = false;
+            }
+        }
+    }
+    
+    if(pass) {
+        cout << "King-God-Emperor" << '\n';
+    } else {
+        cout << "Lier!" << '\n';
+    }
     
     return 0;
 }
