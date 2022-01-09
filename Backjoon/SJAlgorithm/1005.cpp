7fd3106b3f3c78ef546166e763a0d0cdd503e6aa
wi-seong-cheol <tjd9248@naver.com>
2022-01-09 20:52:47 +0900

    1005 solve

--- Backjoon/SJAlgorithm/1005.cpp
+++ Backjoon/SJAlgorithm/1005.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
@@ -15,25 +17,44 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
     
     
-    int t; cin >> t;
-    int D[1000];
-    vector<vector<int>> G;
+    int tc; cin >> tc;
+    int ind[1001], t[1001], dist[1001];
+    vector<int> G[1001];
     
-    for(int i = 0; i < t; i++) {
+    for(int i = 0; i < tc; i++) {
         int N, K; cin >> N >> K;
-        G.resize(K);
+        memset(G, 0, sizeof(G));
+        memset(dist, 0, sizeof(dist));
+        memset(ind, 0, sizeof(ind));
         
-        for(int j = 0; j < N; j++) {
-            cin >> D[j];
+        for(int j = 1; j <= N; j++) {
+            cin >> t[j];
         }
         
         for(int j = 0; j < K; j++) {
-            int x, y; cin >> x >> y;
-            G[x].push_back(y);
-            G[y].push_back(x);
+            int u, v; cin >> u >> v;
+            G[u].push_back(v);
+            ind[v]++;
         }
         int W; cin >> W;
-        
+        queue<int> q;
+        for (int i = 1; i <= N; i++) {
+            if (!ind[i]) { q.push(i); dist[i] = t[i]; }
+        }
+        while (!q.empty()) {
+            int x = q.front(); q.pop();
+            if (x == W) break;
+            
+            for (int nx : G[x]) {
+                if (--ind[nx] == 0) {
+                    q.push(nx);
+                }
+                if (dist[nx] < dist[x] + t[nx]) {
+                    dist[nx] = dist[x] + t[nx];
+                }
+            }
+        }
+        cout << dist[W] << '\n';
     }
     
     return 0;
