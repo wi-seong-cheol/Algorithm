45c6cb480b27581266bad27815eeef51b21c7e7e
wi-seong-cheol <tjd9248@naver.com>
2022-02-01 14:15:56 +0900

    2204 solve

--- Backjoon/SJAlgorithm/2204.cpp
+++ Backjoon/SJAlgorithm/2204.cpp
@@ -6,9 +6,44 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool compare(string a, string b) {
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] >= 'a' && a[i] <= 'z') {
+            a[i] -= 32;
+        }
+    }
+    
+    for(int i = 0; i < b.size(); i++) {
+        if(b[i] >= 'a' && b[i] <= 'z') {
+            b[i] -= 32;
+        }
+    }
+    if(a.compare(b) > 0) return false;
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    while(1) {
+        int N; cin >> N;
+        getchar();
+        if(!N) break;
+        
+        vector<string> v(N);
+        for(int i = 0; i < N; i++) {
+            string s; cin >> s;
+            v[i] = s;
+        }
+        sort(v.begin(), v.end(), compare);
+        cout << v[0] << '\n';
+    }
+    
     return 0;
 }
