ac27897d9db3847d44eb724c18adc0b54c4ae254
wi-seong-cheol <tjd9248@naver.com>
2022-02-01 13:15:56 +0900

    1926 solve

---
+++ Backjoon/SJAlgorithm/2204.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2204
+//
+//  Created by wi_seong on 2022/02/06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
