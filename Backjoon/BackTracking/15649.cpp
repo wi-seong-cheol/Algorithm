623b3e2fb18a58cae61ecede645ca90c82051a72
wi-seong-cheol <tjd9248@naver.com>
2022-02-08 13:28:39 +0900

    15649 solve

---
+++ Backjoon/BackTracking/15649.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  15649
+//
+//  Created by wi_seong on 2022/02/08.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+bool isused[10];
+
+void func(int k) {
+    if(k == m) {
+        for(int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= n; i++) {
+        if(!isused[i]) {
+            arr[k] = i;
+            isused[i] = 1;
+            func(k+1);
+            isused[i] = 0;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    
+    cin >> n >> m;
+    func(0);
+    
+    return 0;
+}
