d61b3ce40903bd21f38629da125d4d4e9d61a48d
wi-seong-cheol <tjd9248@naver.com>
2022-02-10 01:06:43 +0900

    1431 solve

---
+++ Backjoon/Sort/1431.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  1431
+//
+//  Created by wi_seong on 2022/02/10.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int n;
+string st;
+vector<string> v;
+
+bool cmp(const string& a, const string& b) {
+    int lena = a.size(), lenb = b.size();
+    int suma = 0, sumb = 0;
+    
+    if(lena != lenb) return lena < lenb;
+    
+    for(int i = 0; i < lena; i++)
+        if(isdigit(a[i])) suma += a[i] - '0';
+    
+    for(int i = 0; i < lenb; i++)
+        if(isdigit(b[i])) sumb += b[i] - '0';
+    
+    if(suma != sumb) return suma < sumb;
+    
+    return a < b;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> st;
+        v.push_back(st);
+    }
+    sort(v.begin(), v.end(), cmp);
+    for(auto i : v) cout << i << '\n';
+    
+    return 0;
+}
