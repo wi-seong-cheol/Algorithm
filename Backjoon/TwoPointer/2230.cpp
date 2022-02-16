c9992396e30d6e698f349f080346fcf9dca76eb2
wi-seong-cheol <tjd9248@naver.com>
2022-02-16 23:59:51 +0900

    2230 solve

---
+++ Backjoon/TwoPointer/2230.cpp
@@ -0,0 +1,32 @@
+//
+//  2230.cpp
+//  main
+//
+//  Created by wi_seong on 2022/02/16.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n, m;
+int a[100001];
+int mn = 0x7fffffff;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a+n);
+    int en = 0;
+    for(int st = 0; st < n; st++) {
+        while(en < n && a[en] - a[st] < mn) en++;
+        if(en == n) break;
+        mn = min(mn, a[en] - a[st]);
+    }
+    cout << mn;
+    
+    return 0;
+}
