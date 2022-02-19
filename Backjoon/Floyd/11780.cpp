16990e591b6cad93864d3d140d34482251933d7b
wi-seong-cheol <tjd9248@naver.com>
2022-02-19 16:04:34 +0900

    11780 solve

---
+++ Backjoon/Floyd/11780.cpp
@@ -0,0 +1,67 @@
+//
+//  11780.cpp
+//  main
+//
+//  Created by wi_seong on 2022/02/19.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+const int INF = 0x3f3f3f3f;
+int cost[101][101];
+int nxt[101][101];
+int n, m;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++)
+        fill(cost[i], cost[i] + n + 1, INF);
+    while(m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cost[a][b] = min(cost[a][b], c);
+        nxt[a][b] = b;
+    }
+    
+    for(int i = 1; i <= n; i++) cost[i][i] = 0;
+    for(int k = 1; k <= n; k++)
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                if(cost[i][j] > cost[i][k] + cost[k][j]) {
+                    cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
+                    nxt[i][j] = nxt[i][k];
+                }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(cost[i][j] == INF) cout << "0 ";
+            else cout << cost[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(cost[i][j] == 0 || cost[i][j] == INF) {
+                cout << "0\n";
+                continue;
+            }
+            vector<int> path;
+            int st = i;
+            while(st != j) {
+                path.push_back(st);
+                st = nxt[st][j];
+            }
+            path.push_back(j);
+            cout << path.size() << ' ';
+            for(auto x : path) cout << x << ' ';
+            cout << '\n';
+        }
+    }
+    
+    return 0;
+}
