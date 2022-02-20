e958f43272935e2364a557a0e85bf58b2eb51241
wi-seong-cheol <tjd9248@naver.com>
2022-02-20 17:43:41 +0900

    1753 solve

--- Backjoon/ETC/Test.cpp
+++ Backjoon/ETC/Test.cpp
@@ -10,34 +10,32 @@
 using namespace std;
 #define X first
 #define Y second
+vector<pair<int, int>> adj[20005];
+const int INF = 0x3f3f3f3f;
+bool fix[20005];
+int d[20005];
+int V = 10;
 
-void prim() {
-    int v, e;
-
-    vector<pair<int, int>> adj[10005];
-    bool chk[10005];
-    int cnt = 0;
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-    chk[1] = 1;
-    for(auto nxt : adj[1])
-        pq.push({nxt.X, 1, nxt.Y});
-    while(cnt < v - 1) {
-        int cost, a, b;
-        tie(cost, a, b) = pq.top(); pq.pop();
-        if(chk[b]) continue;
-        cout << cost << ' ' << a << ' ' << b << '\n';
-        chk[b] = 1;
-        cnt++;
-        for(auto nxt : adj[b]) {
-            if(!chk[nxt.Y])
-                pq.push({nxt.X, b, nxt.Y});
+void dijkstra_naive(int st) {
+    fill(d, d + V + 1, INF);
+    d[st] = 0;
+    while(true) {
+        int idx = -1;
+        for(int i = 1; i <= V; i++) {
+            if(fix[i]) continue;
+            if(idx == -1) idx = i;
+            else if(d[i] < d[idx]) idx = i;
         }
+        if(idx == -1 || d[idx] == INF) break;
+        fix[idx] = 1;
+        for(auto nxt : adj[idx])
+            d[nxt.Y] = min(d[nxt.Y], d[idx] + nxt.Y);
     }
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
+    
     return 0;
 }
