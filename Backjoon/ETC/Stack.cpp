97e8b8b3915600fa0b98c67a708552a1056f1e7e
wi-seong-cheol <tjd9248@naver.com>
2022-02-21 16:59:35 +0900

    stack

---
+++ Backjoon/ETC/Stack.cpp
@@ -0,0 +1,40 @@
+//
+//  Stack.cpp
+//  main
+//
+//  Created by wi_seong on 2022/02/21.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MX = 1000005;
+int dat[MX];
+int pos = 0;
+
+void push(int x){
+    if(pos < MX) dat[pos++] = x;
+}
+
+void pop(){
+    if(pos > 0) pos--;
+}
+
+int top(){
+    return dat[pos - 1];
+}
+
+void test(){
+    push(5); push(4); push(3);
+    cout << top() << '\n'; // 3
+    pop(); pop();
+    cout << top() << '\n'; // 5
+    push(10); push(12);
+    cout << top() << '\n'; // 12
+    pop();
+    cout << top() << '\n'; // 10
+}
+
+int main(void) {
+    test();
+}
