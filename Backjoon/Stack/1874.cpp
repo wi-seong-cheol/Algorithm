25abea4e07d69b48a63bd0c631eb21eb666f5e85
wi-seong-cheol <tjd9248@naver.com>
2022-02-21 19:18:55 +0900

    1874 solve

---
+++ Backjoon/Stack/1874.cpp
@@ -0,0 +1,41 @@
+//
+//  1874.cpp
+//  main
+//
+//  Created by wi_seong on 2022/02/21.
+//
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+int n;
+string res;
+stack<int> s;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    int cnt = 1;
+    while(n--) {
+        int x; cin >> x;
+        
+        while(cnt <= x) {
+            s.push(cnt++);
+            res += '+';
+        }
+        if(s.top() == x) {
+            s.pop();
+            res += '-';
+        } else {
+            cout << "NO";
+            return 0;
+        }
+    }
+    for(int i = 0; i < res.size(); i++)
+        cout << res[i] << '\n';
+    return 0;
+}
