e6eae2d127127ab8e5ab6647a2abe63959063ca2
wi-seong-cheol <tjd9248@naver.com>
2022-02-25 22:17:33 +0900

    9446 solve

---
+++ Backjoon/BFS/9446.cpp
@@ -0,0 +1,52 @@
+//
+//  9446.cpp
+//  main
+//
+//  Created by wi_seong on 2022/02/25.
+//
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+const int MX = 100'005;
+const int NOT_VISITED = 0;
+const int CYCLE_IN = 1;
+int n;
+int arr[MX];
+int state[MX];
+
+void run(int x) {
+    int cur = x;
+    while(1) {
+        state[cur] = x;
+        cur = arr[cur];
+        if(state[cur] == x) {
+            while(state[cur] != CYCLE_IN) {
+                state[cur] = CYCLE_IN;
+                cur = arr[cur];
+            }
+            return;
+        } else if(state[cur] != 0) return;
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >>t;
+    while(t--) {
+        cin >> n;
+        fill(state, state+n+1, 0);
+        for(int i = 0; i < n; i++)
+            cin >> arr[i];
+        int ans = 0;
+        for(int i = 1; i <= n; i++)
+            if(state[i] == NOT_VISITED) run(i);
+        for(int i = 1; i <= n; i++)
+            if(state[i] != CYCLE_IN) ans++;
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
