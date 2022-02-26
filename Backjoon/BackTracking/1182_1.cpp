08c92c4f8064a8617bf76b47d57e956b3adede0e
wi-seong-cheol <tjd9248@naver.com>
2022-02-27 00:42:02 +0900

    1182 solve

---
+++ Backjoon/BackTracking/1182_1.cpp
@@ -0,0 +1,31 @@
+//
+//  1182_1.cpp
+//  main
+//
+//  Created by wi_seong on 2022/02/27.
+//
+
+#include <iostream>
+using namespace std;
+
+int n, s;
+int arr[30];
+int cnt = 0;
+void func(int cur, int tot){
+    if(cur == n){
+        if(tot == s) cnt++;
+        return;
+    }
+    func(cur+1, tot);
+    func(cur+1, tot+arr[cur]);
+}
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> s;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    func(0, 0);
+    if(s == 0) cnt--;
+    cout << cnt;
+}
