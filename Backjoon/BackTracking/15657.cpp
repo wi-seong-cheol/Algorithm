e90c1ac8089356adee8cd53a9cba88929cc0a1bf
wi-seong-cheol <tjd9248@naver.com>
2022-02-27 18:03:48 +0900

    15657 solve

---
+++ Backjoon/BackTracking/15657.cpp
@@ -0,0 +1,41 @@
+//
+//  15657.cpp
+//  main
+//
+//  Created by wi_seong on 2022/02/27.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n, m;
+int arr[10];
+int num[10];
+
+void func(int k) {
+    if(k == m) {
+        for(int i = 0; i < m; i++)
+            cout << num[arr[i]] << ' ';
+        cout << '\n';
+        return;
+    }
+    int st = 0;
+    if(k != 0) st = arr[k - 1];
+    for(int i = st; i < n; i++) {
+        arr[k] = i;
+        func(k + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> num[i];
+    sort(num, num + n);
+    func(0);
+    
+    return 0;
+}
