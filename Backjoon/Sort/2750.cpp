6fcd67fa4fb66d48b030de865aceb588b7070a53
wi-seong-cheol <tjd9248@naver.com>
2022-03-04 17:23:55 +0900

    2750 solve

---
+++ Backjoon/Sort/2750.cpp
@@ -0,0 +1,31 @@
+//
+//  2750.cpp
+//  main
+//
+//  Created by wi_seong on 2022/03/04.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+const int MX = 2000;
+const int HALF = MX / 2;
+
+int n;
+bool chk[MX + 2];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    while(n--) {
+        int a; cin >> a;
+        chk[a + HALF] = 1;
+    }
+    for(int i = 0; i <= MX; i++)
+        if(chk[i]) cout << i - HALF << '\n';
+    
+    return 0;
+}
