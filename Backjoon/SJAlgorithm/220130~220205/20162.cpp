bc38a94d14cb8d00b66c1ed086b8d423d380394d
wi-seong-cheol <tjd9248@naver.com>
2022-03-13 20:45:34 +0900

    20162 solve

--- Backjoon/SJAlgorithm/220130~220205/20162.cpp
+++ Backjoon/SJAlgorithm/220130~220205/20162.cpp
@@ -6,14 +6,26 @@
 //
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+int n;
+int a[1001];
+int d[1001];
 
 int main() {
     ios::sync_with_stdio(NULL);
     cin.tie(NULL); cout.tie(NULL);
     
-    
-    
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    for(int i = 1; i <= n; i++) {
+        d[i] = a[i];
+        for(int j = 1; j < i; j++) {
+            if(a[i] > a[j] && d[j] + a[i] > d[i])
+                d[i] = d[j] + a[i];
+        }
+    }
+    cout << *max_element(d + 1, d + n + 1);
     return 0;
 }
