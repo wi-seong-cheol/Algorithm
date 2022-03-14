1c3f6e14c2c0ddf44c3bcac9ccec42d75800d9d1
wi-seong-cheol <tjd9248@naver.com>
2022-03-14 21:25:52 +0900

    20154 solve

---
+++ Backjoon/DP/2482.cpp
@@ -0,0 +1,21 @@
+//
+//  2482.cpp
+//  main
+//
+//  Created by wi_seong on 2022/03/13.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//
+    char a[100];
+    cin.getline(a, 10);
+    cout << a;
+    return 0; 
+}
