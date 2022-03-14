7454aa099a2969d3a3e27cf23a45fefaf36c899f
wi-seong-cheol <tjd9248@naver.com>
2022-03-14 22:55:15 +0900

    2482 solve

--- Backjoon/DP/2482.cpp
+++ Backjoon/DP/2482.cpp
@@ -6,16 +6,25 @@
 //
 
 #include <iostream>
-#include <string>
 
 using namespace std;
+const int mod = 1e9 + 3;
+int n, k;
+int d[1001][1001];
 
 int main() {
-//    ios::sync_with_stdio(0);
-//    cin.tie(0);
-//
-    char a[100];
-    cin.getline(a, 10);
-    cout << a;
-    return 0; 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++) d[i][1] = i;
+    for(int i = 4; i <= n; i++) {
+        for(int j = 2; j <= k; j++) {
+            if(j > i / 2) break;
+            d[i][j] = (d[i - 1][j] + d[i - 2][j - 1]) % mod;
+        }
+    }
+    cout << d[n][k];
+    
+    return 0;
 }
