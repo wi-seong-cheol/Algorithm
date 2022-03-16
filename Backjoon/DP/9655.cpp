6fff6488111f34b05a5380ee5630089df76550d7
wi-seong-cheol <tjd9248@naver.com>
2022-03-16 23:47:05 +0900

    9655 solve

---
+++ Backjoon/DP/9655.cpp
@@ -0,0 +1,18 @@
+//
+//  9655.cpp
+//  Algorithm
+//
+//  Created by wi_seong on 2022/03/16.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+  cout << (n % 2 ? "SK" : "CY");
+}
