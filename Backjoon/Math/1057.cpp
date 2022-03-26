5ed9285dd897d96e355ccd15fef309608f12f865
wi-seong-cheol <tjd9248@naver.com>
2022-03-27 00:20:41 +0900

    1057 solve

---
+++ Backjoon/Math/1057.cpp
@@ -0,0 +1,36 @@
+//
+//  1057.cpp
+//  main
+//
+//  Created by wi_seong on 2022/03/26.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, a, b;
+    cin >> n >> a >> b;
+    
+    if (a > b) swap(a, b);
+    int ans = 1;
+    while(n) {
+        if(a % 2 == 1 && a + 1 == b) {
+            cout << ans << '\n';
+            break;
+        }
+        else {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+        }
+        ans++;
+        n /= 2;
+    }
+    
+    return 0;
+}
