deee60d4372fd3d46273e2c3da900ef0cd342e79
wi-seong-cheol <tjd9248@naver.com>
2022-03-31 23:30:08 +0900

    15896 solve

---
+++ Backjoon/Math/15896.cpp
@@ -0,0 +1,20 @@
+//
+//  15896.cpp
+//  main
+//
+//  Created by wi_seong on 2022/03/31.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long n; cin >> n;
+    cout << n * 4;
+    
+    return 0;
+}
