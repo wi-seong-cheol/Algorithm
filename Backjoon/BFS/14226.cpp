3fb5782f181a70edbdbe0aff50b436d5515aa19e
Your Name <tjd9248@naver.com>
2022-07-14 22:00:00 +0000

    14226 solve

--- Backjoon/BFS/14226.cpp
+++ Backjoon/BFS/14226.cpp
@@ -5,4 +5,23 @@
 //  Created by wi_seong on 2022/10/07.
 //
 
-#include <stdio.h>
+/*
+ [Input]
+ 18
+ [Output]
+ 8
+ */
+// 시간복잡도: O()
+// 최악시간:
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> v;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
