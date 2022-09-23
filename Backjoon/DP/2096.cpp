cfcce0d51f75094fab3c1c81cefe19c9518a6b6a
wi-seong-cheol <tjd9248@naver.com>
2022-09-23 21:30:20 +0900

    2096 solve

--- Backjoon/DP/2096.cpp
+++ Backjoon/DP/2096.cpp
@@ -5,53 +5,38 @@
 //  Created by wi_seong on 2022/09/23.
 //
 
-// 시간복잡도: O()
-// 최악 시간:
+// 시간복잡도: O(n)
+// 최악 시간: 100000
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
-int n;
-int board[100001][3];
-vector<int> min_d;
-vector<int> max_d;
+int n, tmp0, tmp2, min_d[3], max_d[3];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    bool debug = false;
         
     cin >> n;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < 3; j++) {
-            cin >> board[i][j];
-        }
-    }
     for(int i = 0; i < 3; i++) {
-        min_d.push_back(board[0][i]);
-        max_d.push_back(board[0][i]);
+        int x; cin >> x;
+        min_d[i] = max_d[i] = x;
     }
     for(int i = 1; i < n; i++) {
+        int x, y, z; cin >> x >> y >> z;
+
         // 최댓값
-        max_d.push_back(max(max_d[0], max_d[1]) + board[i][0]);
-        max_d.push_back(max(max(max_d[0], max_d[1]), max_d[2]) + board[i][1]);
-        max_d.push_back(max(max_d[1], max_d[2]) + board[i][2]);
-        for(int i = 0; i < 3; i++) max_d.erase(max_d.begin());
+        tmp0 = max_d[0]; tmp2 = max_d[2];
+        max_d[0] = max(max_d[0], max_d[1]) + x;
+        max_d[2] = max(max_d[1], max_d[2]) + z;
+        max_d[1] = max(max(tmp0, max_d[1]), tmp2) + y;
         
         // 최솟값
-        min_d.push_back(min(min_d[0], min_d[1]) + board[i][0]);
-        min_d.push_back(min(min(min_d[0], min_d[1]), min_d[2]) + board[i][1]);
-        min_d.push_back(min(min_d[1], min_d[2]) + board[i][2]);
-        for(int i = 0; i < 3; i++) min_d.erase(min_d.begin());
-        
-        if(debug){
-            for(auto d: min_d) {
-                cout << d << ' ';
-            }
-            cout << '\n';
-        }
+        tmp0 = min_d[0]; tmp2 = min_d[2];
+        min_d[0] = min(min_d[0], min_d[1]) + x;
+        min_d[2] = min(min_d[1], min_d[2]) + z;
+        min_d[1] = min(min(tmp0, min_d[1]), tmp2) + y;
     }
     
     cout << max(max(max_d[0], max_d[1]), max_d[2]) << ' ';
