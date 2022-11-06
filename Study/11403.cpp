eb0ea4b44260dac607ee1a0deaaacd596069a501
Your Name <you@your-domain.com>
2022-11-06 12:03:56 +0900

    11403 solve

---
+++ Study/11403.cpp
@@ -0,0 +1,20 @@
+//
+//  11403.cpp
+//  main
+//
+//  Created by wi_seong on 2022/09/06.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n; cin >> n;
+    
+    
+    return 0;
+}
